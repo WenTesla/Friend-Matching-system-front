f8efc981997c266310ed2e52ffe15396d85ba04f
BoWen <WenTesla@163.com>
2022-12-05 21:55:51 +0800

    初始化布局

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,45 @@
+<template>
+
+  <van-nav-bar
+      title="标题"
+      left-text="返回"
+      right-text="按钮"
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+  />
+
+  <div id="content">
+    <template v-if="active === 'index' ">
+      <Index/>
+    </template>
+    <template v-if="active === 'team'">
+      <Team/>
+    </template>
+    <template v-if="active === 'user'">
+
+    </template>
+  </div>
+  <van-tabbar v-model="active" @change="onChange" active-color="#ee0a24" inactive-color="#000">
+    <van-tabbar-item icon="home-o" name="index">主页</van-tabbar-item>
+    <van-tabbar-item icon="search" name="team">队伍</van-tabbar-item>
+    <van-tabbar-item icon="setting-o" name="user">个人</van-tabbar-item>
+  </van-tabbar>
+
+
+</template>
+
+<script setup>
+import Index from "../pages/Index.vue";
+
+const onClickLeft = () => alert('左');
+const onClickRight = () => alert('右');
+
+import {ref} from 'vue';
+import {Toast} from 'vant';
+import Team from "../pages/Team.vue";
+
+const active = ref("index");
+const onChange = (index) => Toast(`标签 ${index}`);
+
+</script>
---
+++ src/pages/Team.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>team</h1>
+</template>
+
+<script>
+export default {
+  name: "Team"
+}
+</script>
+
+<style scoped>
+
+</style>
